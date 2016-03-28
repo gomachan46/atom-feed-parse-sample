f6cc7347134ba11dec0e701f1a38c9550bca0a0c
gomachan46 <shiro.gomachan46@gmail.com>
2016-03-28 16:45:50 +0900

    generate author

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328074350) do
+ActiveRecord::Schema.define(version: 20160328074533) do
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "entries", force: true do |t|
     t.string   "title"
