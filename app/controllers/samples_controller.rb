0f766e13460fe5263591a97a018fdbdfb28f93b5
gomachan46 <shiro.gomachan46@gmail.com>
2016-03-28 19:56:42 +0900

    apiを叩いた風にして結果を取得するところまで

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,4 +1,15 @@
 class SamplesController < ApplicationController
   def index
+    @entry = JSON.parse(sample_response)
+  end
+
+  def sample_response
+    entry = Entry.includes([:authors, :links, :category]).first
+    res = entry.attributes.compact
+    res[:authors] = entry.authors
+    res[:links] = entry.links
+    res[:category] = entry.category
+
+    res.to_json
   end
 end
